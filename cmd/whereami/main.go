a7f252f76d36d91ff9f4c42ed4957caec36d9869
Sergey Yakubovskiy <sergey.yakubovskiy@cenomi.com>
2024-04-01 21:22:37 +0300

    [add-] udpated todo list

--- cmd/whereami/main.go
+++ cmd/whereami/main.go
@@ -12,9 +12,11 @@ func init() {
 }
 
 // TODO:
-// 1.
+// 1. Test running whereami on clean vps with
+// go install github.com/s-yakubovskiy/whereami/cmd/whereami@v1.0.2
+// 2. Make default values for config (if it wasn't found). Create it under ~/.config/whereami/
+// 3. Update documentation and how-to install guide. Also how-to setup IP Quality with api key
 
 func main() {
-	// log.Fatal("version:", Version)
 	cmd.Execute()
 }
